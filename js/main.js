c34973160aa6b66b460dc06d7cbd4aea6477c717
AYYANAR VIGNESH <67584044+ayyanar-vignesh@users.noreply.github.com>
2023-08-29 21:39:24 +0530

    Add files via upload
    just need to test deploy

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function change_word(){
+    document.getElementById('demo1').innerHTML="welcome to javascript world";
+    document.getElementById('demo2').innerHTML="welcome to bootstrap world";
+    document.getElementById('demo3').innerHTML="welcome to python world";
+
+}
